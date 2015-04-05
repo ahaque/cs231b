8d06fa1ef90ed945406415f5c892d64d90d132a5
Fahim Imaduddin Dalvi <fid@qatar.cmu.edu>
2015-04-04 17:37:02 -0700

    Computed U in energy and smoothness. Added debugging options

--- project1/grabcut.py
+++ project1/grabcut.py
@@ -1,8 +1,14 @@
-import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from graph_tool.all import *
+from gmm import GMM
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
-from graph_tool.all import *
+
+# Global constants
+gamma = 50
+beta = 0 # TODO: optimize beta Boykov and Jolly 2001
+
 
 # get_args function
 # Intializes the arguments parser and reads in the arguments from the command
@@ -126,7 +132,7 @@ def get_user_selection(img):
 def initialization(img, bbox, debug=False):
     xmin, ymin, xmax, ymax = bbox
     height, width, _ = img.shape
-    alpha = np.zeros((height, width), dtype=int)
+    alpha = np.zeros((height, width), dtype=np.int8)
 
     for h in range(height): # Rows
         for w in range(width): # Columns
@@ -137,6 +143,12 @@ def initialization(img, bbox, debug=False):
                 # Background
                 alpha[h,w] = 0
 
+    foreground_gmm = GMM(5)
+    background_gmm = GMM(5)
+
+    foreground_gmm.initialize_gmm(img[alpha==1,:])
+    background_gmm.initialize_gmm(img[alpha==0,:])
+
     if debug:
         plt.imshow(alpha*265)
         plt.show()
@@ -144,6 +156,8 @@ def initialization(img, bbox, debug=False):
             for j in xrange(alpha.shape[1]):
                 print alpha[i,j],
             print ''
+
+    return alpha, foreground_gmm, background_gmm
 
 # Currently creates a meaningless graph
 def create_graph():
@@ -156,13 +170,93 @@ def create_graph():
     vprop_vint = g.new_vertex_property("vector<float>")
     vprop_vint[g.vertex(1)] = [1.2, 3.5]
 
+# alpha,k - specific values
+def get_pi(alpha, k, gmms):
+    return gmms[alpha].weights[k]
+
+def get_cov_det(alpha, k, gmms):
+    return gmms[alpha].gaussians[k].sigma_det
+
+def get_mean(alpha, k, gmms):
+    return gmms[alpha].gaussians[k].mean
+
+def get_cov_inv(alpha, k, gmms):
+    return gmms[alpha].gaussians[k].sigma_inv
+
+# Its not log_prob but we are calling it that for convinience
+def get_log_prob(alpha, k, gmms, z_pixel):
+    term = (z_pixel - get_mean(alpha, k, gmms))
+    return 0.5 * np.dot(np.dot(term.T, get_cov_inv(alpha, k, gmms)), term)
+
+def get_energy(alpha, k, gmms, z, smoothness_matrix):
+    # Compute U
+    U = 0
+    for h in xrange(z.shape[0]):
+        for w in xrange(z.shape[1]):
+            U += -np.log(get_pi(alpha[h,w], k[h,w], gmms)) \
+                + 0.5 * np.log(get_cov_det(alpha[h,w], k[h,w], gmms)) \
+                + get_log_prob(alpha[h,w], k[h,w], gmms, z[h,w,:])
+
+    # Compute V
+    V = 0
+    for h in xrange(z.shape[0]):
+        for w in xrange(z.shape[1]):
+            for (nh, nw) in smoothness_matrix[(h,w)].keys():
+                print (h,w), (nh, nw)
+
+def compute_smoothness(z):
+    height, width, _ = z.shape
+    global beta
+    smoothness_matrix = dict()
+
+    for h in xrange(z.shape[0]):
+        print 'In row', h
+        for w in xrange(z.shape[1]):
+            smoothness_matrix[(h,w)] = dict()
+            for hh in [-1,0,1]:
+                for ww in [-1,0,1]:
+                    if hh == 0 and ww == 0:
+                        continue
+                    if w + ww < 0 or w + ww >= width:
+                        continue
+                    if h + hh < 0 or h + hh >= height:
+                        continue
+
+                    smoothness_matrix[(h,w)][(h+hh, w+ww)] = \
+                        np.exp(-1 * beta * np.linalg.norm(z[h,w,:] - z[h+hh,w+ww,:]))
+
+    return smoothness_matrix
+
 def main():
     args = get_args()
     img = load_image(*args.image_file)
     
-    bbox = get_user_selection(img)
+    # bbox = get_user_selection(img)
+    bbox = [10, 10, img.shape[0]-10, img.shape[1]-10]
 
-    initialization(img, bbox)
+    print 'Initializing gmms'
+    alpha, foreground_gmm, background_gmm = initialization(img, bbox)
+    k = np.zeros((img.shape[0],img.shape[1]), dtype=int)
+
+    print 'Computing smoothness matrix...'
+    smoothness_matrix = compute_smoothness(img)
+    
+    print 'Starting EM'
+    while True:
+        # 1. Assigning GMM components to pixels
+        for h in xrange(img.shape[0]):
+            for w in xrange(img.shape[1]):
+                if alpha[h,w] == 1:
+                    k[h,w] = foreground_gmm.get_component(img[h,w,:])
+                else:
+                    k[h,w] = background_gmm.get_component(img[h,w,:])
+
+        # 2. Learn GMM parameters
+        get_energy(alpha, k, (background_gmm, foreground_gmm), img, smoothness_matrix)
+
+        # 3. Estimate segmentation using min cut
+
+        break
 
 if __name__ == '__main__':
     main()
