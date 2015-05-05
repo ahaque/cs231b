0cd9386f1513497b722b2ba3e6cda1111e93a7da
Fahim Imaduddin Dalvi <fid@qatar.cmu.edu>
2015-05-05 02:29:22 -0700

    Finished baseline init implementation

--- project2/code/tld/tldNN.m
+++ project2/code/tld/tldNN.m
@@ -3,41 +3,67 @@
 % This file is part of TLD.
 
 function [conf1,isin] = tldNN(x,tld)
-% 'conf1' ... full model (Relative Similarity)
-% 'isnin' ... inside positive ball, id positive ball, inside negative ball
+    % 'conf1' ... full model (Relative Similarity)
+    % 'isnin' ... inside positive ball, id positive ball, inside negative ball
 
-isin = nan(3,size(x,2));
+    isin = nan(3,size(x,2));
 
-if isempty(tld.pex) % IF positive examples in the model are not defined THEN everything is negative
-    conf1 = zeros(1,size(x,2));
-    return;
+    if isempty(tld.pex) % IF positive examples in the model are not defined THEN everything is negative
+        conf1 = zeros(1,size(x,2));
+        return;
+    end
+
+    if isempty(tld.nex) % IF negative examples in the model are not defined THEN everything is positive
+        conf1 = ones(1,size(x,2));
+        return;
+    end
+
+    conf1 = nan(1,size(x,2));
+
+
+    % ------------------------ (BEGIN) ----------------------------
+    % TODO: set conf1, isin for all columns in x
+    % given
+    %------
+    % x: each column in x corresponds to a patch feature. Let N be number of patches.
+    % tld.pex: positive examples stored in tld so far
+    % tld.nex: negative examples stored in tld so far
+    % output
+    %-------
+    % conf1: a N dimension vector of confidence scores for each patch in x
+    %        One good confidence measure is (1 - max NCC of patch with negs)/(2 - max NCC of patch with negs - max NCC of patch with pos)
+    % isin: size(3, N)
+    %        -- isin(1,i) is set to 1 (else set to nan) if patch i belongs to positives
+    %        -- isin(2,i) index of the maximally correlated positive patch from pex
+    %        -- inin(3,i) is set to 1 (else set to nan) if patch i belongs to negatives 
+    % HINT: Make use of distance function in mex folder to make things faster
+    
+    pEx_dists = -1*ones(1, size(x,2));
+    nEx_dists = -1*ones(1, size(x,2));
+    for i=1:size(x, 2)
+        for p=1:size(tld.pex, 2)
+            pEx_dists(p) = distance(x(:,i), tld.pex(:,p), 1);
+        end
+        for n=1:size(tld.nex, 2)
+            nEx_dists(n) = distance(x(:,i), tld.nex(:,n), 1);
+        end
+        
+        [pEx_value, pEx_index] = max(pEx_dists);
+        [nEx_value, ~] = max(pEx_dists);
+        
+        isin(2,i) = pEx_index;
+        if pEx_value < nEx_value || pEx_value < tld.model.thr_nn
+            % NN was a negative patch - hence curr patch is negative
+            % or pEx_value was lower than threshold
+            isin(1,i) = nan;
+            isin(3,i) = 1;
+        else
+            % NN was a positive patch within threshold
+            isin(1,i) = 1;
+            isin(3,i) = nan;
+        end
+        conf1(i) = (1 - nEx_value)/(2 - nEx_value - pEx_value);
+    end
+
+    % ----------------------- (END) -------------------------------
 end
-
-if isempty(tld.nex) % IF negative examples in the model are not defined THEN everything is positive
-    conf1 = ones(1,size(x,2));
-    return;
-end
-
-conf1 = nan(1,size(x,2));
-
-
-%% ------------------------ (BEGIN) ----------------------------
-%% TODO: set conf1, isin for all columns in x
-%% given
-%%------
-%% x: each column in x corresponds to a patch feature. Let N be number of patches.
-%% tld.pex: positive examples stored in tld so far
-%% tld.nex: negative examples stored in tld so far
-%% output
-%%-------
-%% conf1: a N dimension vector of confidence scores for each patch in x
-%%        One good confidence measure is (1 - max NCC of patch with negs)/(2 - max NCC of patch with negs - max NCC of patch with pos)
-%% isin: size(3, N)
-%%        -- isin(1,i) is set to 1 (else set to nan) if patch i belongs to positives
-%%        -- isin(2,i) index of the maximally correlated positive patch from pex
-%%        -- inin(3,i) is set to 1 (else set to nan) if patch i belongs to negatives 
-%% HINT: Make use of distance function in mex folder to make things faster
-
-
-
-%% ----------------------- (END) -------------------------------
--- project2/code/tld/tldTrainNN.m
+++ project2/code/tld/tldTrainNN.m
@@ -3,25 +3,48 @@
 % This file is part of TLD.
 
 function tld = tldTrainNN(pEx,nEx,tld)
+    nP = size(pEx,2); % get the number of positive example 
+    nN = size(nEx,2); % get the number of negative examples
 
-nP = size(pEx,2); % get the number of positive example 
-nN = size(nEx,2); % get the number of negative examples
+    % ------------------ (BEGIN) --------------------
+    % TODO: Update tld.pex and tld.nex
 
+    % These are the positive and negative examples respectively retained
+    % from all images seen so far.
+    % given:
+    % -----
+    %  (pEx, nEx) -- Your are provided a set of positives and negatives from current image
+    % respectively.
+    %
+    % to update:
+    % ---------
+    %  tld.pex, tld.nex -- Choose a good scheme to update the positives and negatives.
+    %                      Naively extending tld.pex and tld.nex with all of pEx and nEx will
+    %                      blow up computation. Choose wisely!
 
-%% ------------------ (BEGIN) --------------------
-%% TODO: Update tld.pex and tld.nex
+    % Perform random forgetting
+    
+    MAX_NUM_EXAMPLES = 500;
+    num_prev_pos_examples = size(tld.pex, 2);
+    if (num_prev_pos_examples + size(pEx, 2)) > MAX_NUM_EXAMPLES
+        num_to_keep = MAX_NUM_EXAMPLES - size(pEx, 2);
+        num_to_keep = min(num_prev_pos_examples, max(num_to_keep, 0));
+        
+        % Choosing random indices to keep
+        tld.pex = tld.pex(datasample(1:num_prev_pos_examples, num_to_keep, 'Replace', false))
+    end
+    
+    num_prev_neg_examples = size(tld.nex, 2);
+    if (num_prev_neg_examples + size(nEx, 2)) > MAX_NUM_EXAMPLES
+        num_to_keep = MAX_NUM_EXAMPLES - size(nEx, 2);
+        num_to_keep = min(num_prev_neg_examples, max(num_to_keep, 0));
+        
+        % Choosing random indices to keep
+        tld.nex = tld.nex(datasample(1:num_prev_neg_examples, num_to_keep, 'Replace', false))
+    end
+    
+    tld.pex = [tld.pex pEx];
+    tld.nex = [tld.nex nEx];
 
-%% These are the positive and negative examples respectively retained
-%% from all images seen so far.
-%% given:
-%% -----
-%%  (pEx, nEx) -- Your are provided a set of positives and negatives from current image
-%% respectively.
-%%
-%% to update:
-%% ---------
-%%  tld.pex, tld.nex -- Choose a good scheme to update the positives and negatives.
-%%                      Naively extending tld.pex and tld.nex with all of pEx and nEx will
-%%                      blow up computation. Choose wisely!
-
-%% ------------------ (END) ----------------------
+    % ------------------ (END) ----------------------
+end
