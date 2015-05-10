82cd76e402d0fbba42709847a1878d5bd24ba51e
Albert <ahaque@cs.stanford.edu>
2015-05-10 13:44:40 -0700

    Added more datasets

--- project2/code/features/extractFeaturesFromPatches.m
+++ project2/code/features/extractFeaturesFromPatches.m
@@ -7,6 +7,8 @@ function [ features ] = extractFeaturesFromPatches( tld, patches )
         features = extractRaw(patches, tld.model.patchsize);
     elseif strcmp('hog', tld.detection_model_params.feature) == 1
         features = extractHOG(patches, tld.model.patchsize);
+    elseif strcmp('brisk', tld.detection_model_params.feature) == 1
+        features = extractBRISK(patches, tld.model.patchsize);
     end
     
 end
