8a696f45b6b584207731a5bdbd03e1eb07f3d339
Albert Haque <ahaque@cs.stanford.edu>
2015-05-10 12:44:53 -0700

    Added HOG feature extraction

---
+++ project2/code/features/extractHOG.m
@@ -0,0 +1,19 @@
+
+% Given a cell array of patches, extract features
+% Input: patches: cell array of size n x 1 containing a 2D matrix for each patch
+% Output: features: a matrix where the ith column is the features for patch i
+function [ features ] = extractHOG( patches, patchsize )
+        
+    for i = 1:size(patches, 1)
+        patch = fastResize(patches{i}, patchsize);
+        
+        if i >= 1
+            features(:,i) = extractHOGFeatures(patch);
+        else
+            feat = extractHOGFeatures(patch);
+            features = zeros(size(feat,2), size(patches, 1));
+            features(:,1) = feat;
+        end
+    end
+end
+
