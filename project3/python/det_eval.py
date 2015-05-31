bb021502847f687a20061b1e202ca4aa3ee9f9bc
Fahim Imaduddin Dalvi <fid@qatar.cmu.edu>
2015-05-31 16:48:56 -0700

    Ported evaluation code to python. Added test cases against matlab

---
+++ project3/python/det_eval.py
@@ -0,0 +1,109 @@
+import numpy as np
+import util
+
+def det_eval(gt_bboxes, pred_bboxes):
+    # det_eval
+    # Arguments:
+    # gt_bboxes: Cell array of ground truth bounding boxes for a single class.
+    #   Each element corresponds to a single image and is a matrix of dimensions
+    #   n x 5, where n is the number of bounding boxes. Each bounding box is
+    #   represented as [x1 y1 x2 y2 score], where 'score' is the detection score.
+    # pred_bboxes: Cell array of predicted bounding boxes in the same format as
+    #   gt_bboxes, without the score component.
+    #
+    # Returns:
+    #   ap: average precision
+    #   prec: The precision at each point on the PR curve
+    #   rec: The recall at each point on the PR curve.
+    minoverlap = 0.5
+    assert(len(gt_bboxes) == len(pred_bboxes))
+    num_images = len(gt_bboxes)
+
+    num_gt_boxes = np.sum([x.shape[0] for x in gt_bboxes])
+    num_pred_boxes = np.sum([x.shape[0] for x in pred_bboxes])
+    [all_pred_bboxes, image_ids] = sort_bboxes(pred_bboxes)
+
+    tp = np.zeros((num_pred_boxes, 1))
+    fp = np.zeros((num_pred_boxes, 1))
+
+    detected_pred_bboxes = [None]*num_images
+    detected = [None]*num_images
+
+    for pred_ind in xrange(num_pred_boxes):
+        # if mod(pred_ind, 4096) == 0:
+        #     fprintf(mstring('calculating detection box %d of %d\\n'), pred_ind, num_pred_boxes)
+        # end
+        num_gt_im_boxes = len(gt_bboxes[image_ids[pred_ind]])
+        if detected[image_ids[pred_ind][0]] is None:
+            detected[image_ids[pred_ind][0]] = np.zeros((num_gt_im_boxes, 1))
+        
+        bb = all_pred_bboxes[pred_ind, :]
+
+        overlaps = np.zeros((num_gt_im_boxes))
+        
+        for g in xrange(num_gt_im_boxes):
+            gt_bbox = gt_bboxes[image_ids[pred_ind]][g,:]
+            overlaps[g] = util.computeOverlap(gt_bbox, np.array([bb]))
+
+        jmax = np.argmax(overlaps, axis=0)
+        ovmax = overlaps[jmax]
+
+        # assign detection as true positive/don't care/false positive
+        im_detected = detected[image_ids[pred_ind]]
+        if ovmax >= minoverlap:
+            if not im_detected[jmax]:
+                tp[pred_ind] = 1            #true positive
+                detected[image_ids[pred_ind]][jmax] = 1
+                if detected_pred_bboxes[image_ids[pred_ind]] is None:
+                    detected_pred_bboxes[image_ids[pred_ind]]=np.array([bb])
+                else:
+                    detected_pred_bboxes[image_ids[pred_ind]]=util.stack([detected_pred_bboxes[image_ids[pred_ind]], bb]); 
+            else:
+                fp[pred_ind] = 1            #false positive (multiple detections)
+        else:
+            fp[pred_ind] = 1        #false positive
+
+
+    # compute precision/recall
+    npos = num_gt_boxes
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+
+    rec = 1.0*tp / npos
+    prec = np.divide(tp, (fp + tp))
+    ap = VOCap(rec, prec)
+
+    return ap, prec, rec
+
+def sort_bboxes(pred_bboxes):
+    num_pred_boxes = np.sum([x.shape[0] for x in pred_bboxes])
+
+    bboxes = util.stack([b for b in pred_bboxes if b.size > 0])
+    image_ids = np.zeros((bboxes.shape[0], 1), dtype=int)
+
+    # Concatenate them
+    bbox_ind = 0
+    for im_ind in xrange(len(pred_bboxes)):
+        num_bbox_curr_image = pred_bboxes[im_ind].shape[0]
+        image_ids[bbox_ind:bbox_ind + num_bbox_curr_image, 0] = im_ind
+        bbox_ind = bbox_ind + num_bbox_curr_image
+
+    # Sort
+    IDX = np.argsort(-1*bboxes[:, -1])
+    bboxes = bboxes[IDX, :]
+    image_ids = image_ids[IDX]
+
+    return bboxes, image_ids
+
+
+def VOCap(rec, prec):
+    mrec = np.concatenate((np.array([0]), rec, np.array([1])))
+    mpre = np.concatenate((np.array([0]), prec, np.array([0])))
+
+    for i in xrange(len(mpre)-2,-1,-1):
+        mpre[i] = max(mpre[i], mpre[i + 1])
+    
+    i = np.where(mrec[1:] != mrec[0:-1])[0] + 1;
+    ap = np.sum(np.multiply((mrec[i]-mrec[i-1]), mpre[i]))
+
+    return ap
--- project3/python/tester.py
+++ project3/python/tester.py
@@ -1,18 +1,120 @@
 import util
 import numpy as np
+import det_eval
+import random
+try:
+    import matlab.engine
+    run_eval_test = True
+except ImportError:
+    print 'Warning: Matlab python engine not found. Some tests will not be run.'
+    run_eval_test = False
+
+TOLERANCE = 1e-7
 
 def main():
-	########## BBOX overlap test ##########
-	region_bbox = np.array([50, 50, 100, 100])
-	gt_bboxes = np.array([[75, 25, 125, 75], # 0.14
-						[50, 200, 100, 250], # 0 
-						[50, 50, 100, 100] # 1
-						])
-	out = util.computeOverlap(region_bbox, gt_bboxes)
-	assert(np.abs(out[0] - 0.14935926) <= 1e-7)
-	assert(np.abs(out[1] - 0) <= 1e-7)
-	assert(np.abs(out[2] - 1) <= 1e-7)
-	print "Passed all tests."
+    bbox_overlap_test()
+    if run_eval_test:
+        eval_test()
+
+def bbox_overlap_test():
+    print 'Testing BBOX overlap...'
+    ########## BBOX overlap test ##########
+    region_bbox = np.array([50, 50, 100, 100])
+    gt_bboxes = np.array([[75, 25, 125, 75], # 0.14
+                        [50, 200, 100, 250], # 0 
+                        [50, 50, 100, 100] # 1
+                        ])
+    out = util.computeOverlap(region_bbox, gt_bboxes)
+    assert(np.abs(out[0] - 0.14935926) <= TOLERANCE)
+    assert(np.abs(out[1] - 0) <= TOLERANCE)
+    assert(np.abs(out[2] - 1) <= TOLERANCE)
+    print "Passed all tests."
+
+def eval_test(show_output=False):
+    print 'Testing evalutation code...'
+    num_images = random.randint(4,10)
+    gt_bboxes = []
+    pred_bboxes = []
+
+    for i in xrange(num_images):
+        if show_output: print 'Image %d'%(i+1)
+        num_gt_bboxes = random.randint(0,5)
+        num_pred_bboxes = random.randint(0,5)
+
+        gt_bboxes.append([])
+        for g in xrange(num_gt_bboxes):
+            start_x = random.randint(0,300)
+            start_y = random.randint(0,300)
+            end_x = random.randint(start_x + 10,500)
+            end_y = random.randint(start_y + 10,500)
+            gt_bboxes[-1].append([start_x, start_y, end_x, end_y])
+            if show_output: print 'GT: [%d,%d,%d,%d]'%(start_x, start_y, end_x, end_y)
+
+        pred_bboxes.append([])
+        for g in xrange(num_gt_bboxes):
+            start_x = random.randint(0,300)
+            start_y = random.randint(0,300)
+            end_x = random.randint(start_x + 10,500)
+            end_y = random.randint(start_y + 10,500)
+            score = random.random()
+            pred_bboxes[-1].append([start_x, start_y, end_x, end_y, score])
+            if show_output: print 'PRED: [%d,%d,%d,%d]'%(start_x, start_y, end_x, end_y)
+    
+    # gt_bboxes = [[[100,91,160,500],
+    #                     [268,176,307,467],
+    #                     [62,112,491,219],
+    #                     [122,230,394,332],
+    #                     [50,84,319,109]],
+    #             [[253,225,308,262],
+    #                     [145,182,425,329],
+    #                     [220,96,424,407],
+    #                     [12,194,128,365]]]
+    # pred_bboxes = [[[63,2,84,494, 1.0],
+    #                     [80,73,403,180, 1.0],
+    #                     [34,282,445,487, 1.0],
+    #                     [92,129,218,311, 1.0],
+    #                     [299,19,468,94, 1.0],
+    #                     [264,56,461,371, 1.0]],
+    #             [[204,47,247,175, 1.0],
+    #                     [227,180,374,373, 1.0],
+    #                     [224,153,425,493, 1.0],
+    #                     [192,159,467,218, 1.0],
+    #                     [262,176,473,343, 1.0]]]
+
+    ap, prec, rec = det_eval.det_eval([np.array(x) for x in gt_bboxes], [np.array(x) for x in pred_bboxes])
+    if show_output:
+        print 'Python Average precision:', ap
+        print 'Python Precision:',prec
+        print 'Python Recall:',rec
+
+    ### Run Matlab code ###
+    # MATLAB_PATH = '/Applications/MATLAB_R2015a.app/bin/matlab'
+    # OPTIONS = '-nodisplay -nosplash -nodesktop -r "det_eval({[100,91,160,500;268,176,307,467;62,112,491,219;122,230,394,332;50,84,319,109],[253,225,308,262;145,182,425,329;220,96,424,407;12,194,128,365]}, {[63,2,84,494, 1.0; 80,73,403,180, 1.0; 34,282,445,487, 1.0; 92,129,218,311, 1.0; 299,19,468,94, 1.0; 264,56,461,371, 1.0],[204,47,247,175, 1.0;227,180,374,373, 1.0;224,153,425,493, 1.0;192,159,467,218, 1.0;262,176,473,343, 1.0]}); exit"'
+    # command = [MATLAB_PATH, '-nodisplay', '-nosplash', '-nodesktop', '-r','det_eval({[100,91,160,500;268,176,307,467;62,112,491,219;122,230,394,332;50,84,319,109],[253,225,308,262;145,182,425,329;220,96,424,407;12,194,128,365]}, {[63,2,84,494, 1.0; 80,73,403,180, 1.0; 34,282,445,487, 1.0; 92,129,218,311, 1.0; 299,19,468,94, 1.0; 264,56,461,371, 1.0],[204,47,247,175, 1.0;227,180,374,373, 1.0;224,153,425,493, 1.0;192,159,467,218, 1.0;262,176,473,343, 1.0]}); exit']
+    # process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # output = process.communicate()[0]
+    # print output
+    eng = matlab.engine.start_matlab()
+    eng.cd(r'../starter_code/', nargout=0)
+    [m_ap, m_prec, m_rec] = eng.det_eval([matlab.double(x) for x in gt_bboxes], [matlab.double(x) for x in pred_bboxes], nargout=3)
+    m_prec = np.squeeze(np.array(m_prec))
+    m_rec = np.squeeze(np.array(m_rec))
+
+    if show_output:
+        print 'Matlab Average precision:', m_ap
+        print 'Matlab Precision:', m_prec
+        print 'Matlab Recall:', m_rec
+
+    assert(np.abs(ap - m_ap) <= TOLERANCE)
+    assert(prec.size == m_prec.size)
+    for i in xrange(prec.size):
+        assert(np.abs(prec[i] - m_prec[i]) <= TOLERANCE)
+    assert(rec.shape[0] == m_rec.shape[0])
+    for i in xrange(rec.size):
+        assert(np.abs(rec[i] - m_rec[i]) <= TOLERANCE)
+    
+    print "Passed all tests."
+     
 
 if __name__ == '__main__':
-	main()
+    main()
